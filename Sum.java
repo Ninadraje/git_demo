c81c521259322b9325d5de33b7324bd038445f15
raje <Ninad.raje@finastra.com>
2022-01-05 13:07:31 +0530

    latest commit

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
     public static void main(String args[]){
         int a=6;
         int b=8;
-        system.out.println(a+b);
+        system.out.println("Sum : "+ (a+b));
     }
 
 }
